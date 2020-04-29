c8742cf56181075bc90fd808d73c4221622b0b92
gvgvgvijayan <gvgvgvijayan@gmail.com>
2020-04-29 11:56:49 +0530

    v3 Added new css to add some UI improvements

--- vg-infinite-scroll/templates/vg-posts-content.php
+++ vg-infinite-scroll/templates/vg-posts-content.php
@@ -7,9 +7,11 @@ if ($vg_posts_ui_query->have_posts()) {
 ?>
         <article class="vg-post-excerpt vg-item">
             <?php if (has_post_thumbnail()) : ?>
-                <a href="<?php the_permalink(); ?>" alt="<?php the_title_attribute(); ?>">
-                    <?php the_post_thumbnail('medium'); ?>
-                </a>
+                <div class="vg-thumbnail">
+                    <a href="<?php the_permalink(); ?>" alt="<?php the_title_attribute(); ?>">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </a>
+                </div>
             <?php endif; ?>
             <h2 class="vg-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <div class="vg-excerpt-content"><?php the_excerpt(); ?></div>
